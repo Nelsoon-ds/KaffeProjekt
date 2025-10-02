e6eed4792d446297c972be16d522da408af9fe5f
Nelsoon-ds <davidgaming108@gmail.com>
2025-10-02 09:43:47 +0200

    Udgangspunktet 1)

---
+++ src/CoffeeProject/Size.java
@@ -0,0 +1,9 @@
+package CoffeeProject;
+
+// Enum for størrelser
+public enum Size {
+
+        SMALL, MEDIUM, LARGE
+        // TODO: Overvej at tilføje metoder, fx til at få en venlig tekststreng
+
+}
